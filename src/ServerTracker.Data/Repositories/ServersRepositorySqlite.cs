7205de6b5b15e2ac97d141d9c55d29e3f371794d
Josh Kato <joshkato01@gmail.com>
2019-06-14 03:07:49 -0400

    Implement SQLite for ServersRepository

--- src/ServerTracker.Data/Repositories/ServersRepositorySqlite.cs
+++ src/ServerTracker.Data/Repositories/ServersRepositorySqlite.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using ServerTracker.Data.Models;
 
 namespace ServerTracker.Data.Repositories
@@ -15,34 +17,110 @@ namespace ServerTracker.Data.Repositories
             ConnectionString = connectionString;
         }
 
-        public Task CreateServer(Server server)
+        public async Task CreateServer(Server server)
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                INSERT INTO [servers] (
+                    [EnvironmentId],
+                    [Name],
+                    [DomainName],
+                    [IpAddress],
+                    [OperatingSystem]
+                ) VALUES (
+                    @" + nameof(Server.EnvironmentId) + @",
+                    @" + nameof(Server.Name) + @",
+                    @" + nameof(Server.DomainName) + @",
+                    @" + nameof(Server.IpAddress) + @",
+                    @" + nameof(Server.OperatingSystem) + @"
+                );";
+
+            using (var conn = GetConnection())
+            {
+                await conn.ExecuteAsync(sql, server).ConfigureAwait(false);
+            }
         }
 
-        public Task<List<Server>> GetAllServers()
+        public async Task<List<Server>> GetAllServers()
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                SELECT *
+                  FROM [servers]
+                ;";
+
+            List<Server> servers;
+            using (var conn = GetConnection())
+            {
+                var result = await conn.QueryAsync<Server>(sql).ConfigureAwait(false);
+                servers = result.ToList();
+            }
+
+            return servers;
         }
 
-        public Task<Server> GetServer(long id)
+        public async Task<Server> GetServer(long id)
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                SELECT *
+                  FROM [servers]
+                 WHERE [Id] = @" + nameof(id) + @"
+                ;";
+
+            Server server;
+            using (var conn = GetConnection())
+            {
+                server = await conn.QueryFirstOrDefaultAsync<Server>(sql, new {id}).ConfigureAwait(false);
+            }
+
+            return server;
         }
 
-        public Task<List<Server>> GetServersForEnvironment(long envId)
+        public async Task<List<Server>> GetServersForEnvironment(long envId)
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                SELECT *
+                  FROM [servers]
+                 WHERE [EnvironmentId] = @" + nameof(envId) + @"
+                ;";
+
+            List<Server> servers;
+            using (var conn = GetConnection())
+            {
+                var result = await conn.QueryAsync<Server>(sql, new {envId}).ConfigureAwait(false);
+                servers = result.ToList();
+            }
+
+            return servers;
         }
 
-        public Task RemoveServer(long id)
+        public async Task RemoveServer(long id)
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                DELETE FROM [servers]
+                      WHERE [Id] = @" + nameof(id) + @"
+                ;";
+
+            using (var conn = GetConnection())
+            {
+                await conn.ExecuteAsync(sql, new {id}).ConfigureAwait(false);
+            }
         }
 
-        public Task UpdateServer(Server server)
+        public async Task UpdateServer(Server server)
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                UPDATE [servers]
+                   SET [EnvironmentId]   = @" + nameof(Server.EnvironmentId) + @",
+                       [Name]            = @" + nameof(Server.Name) + @",
+                       [DomainName]      = @" + nameof(Server.DomainName) + @",
+                       [IpAddress]       = @" + nameof(Server.IpAddress) + @",
+                       [OperatingSystem] = @" + nameof(Server.OperatingSystem) + @"
+                 WHERE [Id] = @" + nameof(Server.Id) + @"
+                ;";
+
+            using (var conn = GetConnection())
+            {
+                await conn.ExecuteAsync(sql, server).ConfigureAwait(false);
+            }
         }
 
         private SQLiteConnection GetConnection()
